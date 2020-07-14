4bae7bcddd54c95baddb469a4891b13b21928166
naamoonoo <naamoonoo.y@gmail.com>
2020-07-14 11:19:16 +0900

    [#4] feat - 재사용 가능한 element warpper 분리

--- client/src/utils/defaultElements.ts
+++ client/src/utils/defaultElements.ts
@@ -1,9 +1,10 @@
 import { IAttribute, generateElement } from './generateElement'
 
-const tagNames = ['div']
+const tagNames = ['div', 'p']
 
 export const [
   div,
+  p,
 ] = tagNames.map(
   (tagName) => (attributes: IAttribute, ...childNodes: HTMLElement[]) =>
     generateElement(tagName, attributes, ...childNodes)
