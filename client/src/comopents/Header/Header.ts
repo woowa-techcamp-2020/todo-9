4655890b3044ab90537c888733187cbd76774a58
naamoonoo <naamoonoo.y@gmail.com>
2020-07-15 14:57:59 +0900

    [#6] feat - add Header Component

---
+++ client/src/comopents/Header/Header.ts
@@ -0,0 +1,42 @@
+import { Component } from '../../utils/Component'
+import { header, div, span, button } from '../../utils/defaultElements'
+import { textSpanIntersectsWithTextSpan } from 'typescript'
+
+interface IProps {
+  title: string
+  onClickMenu: () => void
+}
+
+interface IState {}
+
+export class Header extends Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props)
+
+    Object.setPrototypeOf(this, Header.prototype)
+    this.init()
+  }
+
+  // onClickMenu() {
+  //   this.setState('menuVisible', !this.getState('menuVisible'))
+  // }
+
+  render() {
+    const {
+      props: { title, onClickMenu },
+    } = this
+
+    return header(
+      {},
+      div(
+        { className: 'title-container' },
+        span({ className: 'title-content', textContent: title })
+      ),
+      button({
+        className: 'menu-container',
+        textContent: 'menu',
+        click: () => onClickMenu(),
+      })
+    )
+  }
+}
