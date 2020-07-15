3f9cecbcd49044d374faa7b6759844c60988e4fe
Donguk Hong <mbxd1@naver.com>
2020-07-15 17:45:40 +0900

    [#7] feat: express - db connect

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,10 @@
+import dotenv from 'dotenv'
+import { getConnection } from './config/db'
 import { app } from './app'
+
+dotenv.config()
+
 const PORT = 3000
 
+getConnection()
 app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`))
