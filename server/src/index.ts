5ae01ab96cdaed64b7c21e39322c48d8b1246eac
Donguk Hong <mbxd1@naver.com>
2020-07-15 21:06:23 +0900

    [#7] feat: 환경변수에 따라서 다른 db 참조

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,7 @@
 import dotenv from 'dotenv'
-import { getConnection } from './config/db'
 import { app } from './app'
 
 dotenv.config()
-
 const PORT = 3000
 
-getConnection()
 app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`))
