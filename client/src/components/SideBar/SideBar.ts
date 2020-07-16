ed66c77f057cdd25bc66d5418fe003c468ea1d22
Donguk Hong <mbxd1@naver.com>
2020-07-16 22:36:41 +0900

    [#16] feat - side bar mark up

--- client/src/components/SideBar/SideBar.ts
+++ client/src/components/SideBar/SideBar.ts
@@ -1,14 +1,10 @@
 import { Component } from '../../utils/wooact'
-import { aside, div, i } from '../../utils/wooact/defaultElements'
+import { aside, div, i, span, q, img } from '../../utils/wooact/defaultElements'
 
 interface IProps {}
 interface IState {}
 
 class SideBar extends Component<IProps, IState> {
-  // constructor(props: IProps) {
-  // super(props)
-  // constructor(props: IProps, state: IState) {
-  //   super(props, state)
   constructor() {
     super()
 
@@ -23,7 +19,70 @@ class SideBar extends Component<IProps, IState> {
         { className: 'icon-container' },
         div(
           { className: 'left-icon' },
-          i({ className: 'f7-icons color-black', textContent: 'house' })
+          i({ className: 'f7-icons f7-icon', textContent: 'menu' }),
+          span({ className: 'icon-title', textContent: 'Menu' })
+        ),
+        i({ className: 'f7-icons f7-icon', textContent: 'multiply' })
+      ),
+      div(
+        { className: 'icon-container' },
+        div(
+          { className: 'left-icon' },
+          i({ className: 'f7-icons f7-icon', textContent: 'bell_fill' }),
+          span({ className: 'icon-title', textContent: 'Activity' })
+        )
+      ),
+      // item 없는 경우 white-bg 설정
+      div(
+        { className: 'log-item' },
+        img({
+          className: 'bg-image',
+          // src: '../../../public/images/woowabros.png',
+          // alt: 'woowa-bros',
+        }),
+        div(
+          { className: 'log-contents-container' },
+          div(
+            {},
+            q({ className: 'author', textContent: '@donguk' }),
+            span({ className: 'type', textContent: ' moved' }),
+            span({
+              className: 'content',
+              textContent:
+                ' js공부하고 복습하고 공부하고 복습하고 공부하고 또 복습하고 공부하고 테스트코드 짜고 복습하고 길게 쓰자',
+            }),
+            span({
+              className: 'place',
+              textContent: ' from 해야할일 to 하는중',
+            })
+          ),
+          div({ className: 'updatedAt', textContent: '3minutes ago' })
+        )
+      ),
+      div(
+        { className: 'log-item' },
+        img({
+          className: 'bg-image',
+          // src: '../../../public/images/woowabros.png',
+          // alt: 'woowa-bros',
+        }),
+        div(
+          { className: 'log-contents-container' },
+          div(
+            {},
+            q({ className: 'author', textContent: '@donguk' }),
+            span({ className: 'type', textContent: ' moved' }),
+            span({
+              className: 'content',
+              textContent:
+                ' js공부하고 복습하고 공부하고 복습하고 공부하고 또 복습하고 공부하고 테스트코드 짜고 복습하고 길게 쓰자',
+            }),
+            span({
+              className: 'place',
+              textContent: ' from 해야할일 to 하는중',
+            })
+          ),
+          div({ className: 'updatedAt', textContent: '3minutes ago' })
         )
       )
     )
--- client/src/utils/wooact/generateElement.ts
+++ client/src/utils/wooact/generateElement.ts
@@ -7,6 +7,7 @@ type EventHandler = {
 
 export interface IAttribute extends Partial<Element>, EventHandler {
   text?: string
+  src?: string
 }
 
 export const generateElement = (
