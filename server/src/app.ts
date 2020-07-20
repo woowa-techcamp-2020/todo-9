67ba6fa7ca3c379ad1edc3fc3eb9685ee007966b
naamoonoo <naamoonoo.y@gmail.com>
2020-07-20 21:00:15 +0900

    [#28] chore: udpate middlewares

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,20 @@
 import express from 'express'
 import apis from './apis'
+import bodyParser from 'body-parser'
+import path from 'path'
 
 const app = express()
 
+// middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// api
 app.use('/apis', apis)
 
-app.get('/*', (req, res) => {
-  res.send('hello')
-})
+// app.get('/*', (req, res) => {
+//   // res.sendFile(__dirname + '/public/index.html')
+//   res.sendFile(path.join(__dirname, '/public/index.html'))
+// })
 
 export { app }
--- server/src/schema/index.ts
+++ server/src/schema/index.ts
@@ -1,1 +1,2 @@
 export { default as user } from './User'
+export { default as item } from './item'
