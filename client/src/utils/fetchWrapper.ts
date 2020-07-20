bacbeacbeae533ec916cf0e8859e3e5698531c8a
naamoonoo <naamoonoo.y@gmail.com>
2020-07-20 16:07:59 +0900

    [#25] feature: add api fetch wrapper

---
+++ client/src/utils/fetchWrapper.ts
@@ -0,0 +1,22 @@
+export type MethodType = 'GET' | 'POST' | 'DELETE' | 'PATCH'
+
+export const fetchWrapper = async <T, B>(
+  method: MethodType,
+  url: string,
+  body?: B
+): Promise<T> => {
+  try {
+    const baseUrl = '/api'
+    const response = await fetch(baseUrl + url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+    const res = await response.json()
+    return res
+  } catch (err) {
+    throw new Error(err)
+  }
+}
