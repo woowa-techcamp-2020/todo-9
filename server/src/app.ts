a758c846f14eae4ee725175b10108be8dbbdc7d6
andy nam <naamoonoo.y@gmail.com>
2020-07-21 19:07:18 +0900

    [#28] feat: add cors for dev, set static folder

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,20 +1,26 @@
 import express from 'express'
 import apis from './apis'
 import bodyParser from 'body-parser'
-import path from 'path'
+import appRoot from 'app-root-path'
+import cors from 'cors'
 
 const app = express()
 
 // middleware
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // api
 app.use('/api', apis)
 
-// app.get('/*', (req, res) => {
-//   // res.sendFile(__dirname + '/public/index.html')
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// })
+app.use(express.static(appRoot.resolve('/src/public')))
+
+app.get('/*', (req, res) => {
+  // res.sendFile(__dirname + '/public/index.html')
+  res.sendFile(appRoot.resolve('/src/public/index.html'))
+
+  //   res.sendFile(path.join(__dirname, '/public/index.html'))
+})
 
 export { app }
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,13 @@
 import dotenv from 'dotenv'
+dotenv.config()
 import { app } from './app'
 import { Database } from './schema/Database'
 import { getConnection } from './config/db'
-
+import { uuid } from 'uuidv4'
 const PORT = 3000
 
 const start = async () => {
   try {
-    dotenv.config()
     Database.connectedDB = await getConnection()
 
     app.listen(PORT, () => console.log(`listening on http://localhost:${PORT}`))
