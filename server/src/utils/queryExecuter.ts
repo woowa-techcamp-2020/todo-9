aadae5317057e48863d7cdface945239aea4191c
naamoonoo <naamoonoo.y@gmail.com>
2020-07-22 12:59:41 +0900

    [#33]feat-add queryWrapper on each case with type

--- server/src/utils/queryExecuter.ts
+++ server/src/utils/queryExecuter.ts
@@ -1,6 +1,40 @@
 import { promiseHandler } from './promiseHandler'
+import { getConnection } from 'src/config/db'
 
-export const queryExecuter = async (conn: any, query: string) => {
-  const [[result, _], error] = await promiseHandler(conn.execute(query))
-  return [result, error]
+export type MysqlInsertOrUpdateResult = {
+  fieldCount: number
+  affectedRows: number
+  insertId: number
+  serverStatus: number
+  warningCount: number
+  message: string
+  protocol41: boolean
+  changedRows: number
+}
+
+export const selectQueryExecuter = async <T>(
+  query: string
+): Promise<[T[], any]> => {
+  const { execute } = getConnection()
+
+  const [[result, _], error] = await promiseHandler(execute(query))
+  return [result as T[], error]
+}
+
+export const inserQueryExecuter = async (
+  query: string
+): Promise<[number, any]> => {
+  const { execute } = getConnection()
+
+  const [{ insertId }, error] = await promiseHandler(execute(query))
+  return [insertId, error]
+}
+
+export const updateQueryExecuter = async (
+  query: string
+): Promise<[number, any]> => {
+  const { execute } = getConnection()
+
+  const [{ affectedRows }, error] = await promiseHandler(execute(query))
+  return [affectedRows, error]
 }
