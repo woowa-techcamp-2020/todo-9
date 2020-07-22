7cdeafce6991c8f9c714aab356c17ba5123d2f6e
naamoonoo <naamoonoo.y@gmail.com>
2020-07-22 23:27:56 +0900

    [#36] feature : drag and drop done

--- client/src/utils/dragAndDrop.ts
+++ client/src/utils/dragAndDrop.ts
@@ -1,13 +1,25 @@
 import { transform } from 'typescript'
 
+const position = { x: 0, y: 0 }
 let clicked: boolean = false
 let target: HTMLElement = null
 let cloned = null
-const position = { x: 0, y: 0 }
 let overlappedElement = { item: null, column: null }
+let isOnTop: boolean = false
 
-let originalLeft = null
-let originalTop = null
+const setFloatingItem = (e: MouseEvent, init: boolean = false) => {
+  const { pageX, pageY } = e
+
+  const floatingItem = document.querySelector('.float-item') as HTMLElement
+
+  floatingItem.style.left = pageX + position.x + 'px'
+  floatingItem.style.top = pageY + position.y + 'px'
+
+  if (init) {
+    floatingItem.appendChild(cloned)
+  }
+  return floatingItem
+}
 
 const onMouseDown = (e: MouseEvent) => {
   target = (e.target as HTMLElement).closest('.item-wrapper')
@@ -24,12 +36,7 @@ const onMouseDown = (e: MouseEvent) => {
   position.x = target.offsetLeft - pageX
   position.y = target.offsetTop - pageY
 
-  const floatingItem = document.querySelector('.float-item') as HTMLElement
-
-  floatingItem.style.left = e.pageX + position.x + 'px'
-  floatingItem.style.top = e.pageY + position.y + 'px'
-  cloned.classList.add('item-wrapper')
-  floatingItem.appendChild(cloned)
+  setFloatingItem(e, true)
 }
 
 const onMouseUp = (e: MouseEvent) => {
@@ -39,8 +46,12 @@ const onMouseUp = (e: MouseEvent) => {
   const { pageX, pageY } = e
 
   // if has something overlapped
-  swapIfOverlappedWithItem()
-  moveToOtherColumn()
+  const isItemSwapped = swapIfOverlappedWithItem()
+  const isMovedToOhterColumn = moveToOtherColumn()
+
+  if (isItemSwapped || isMovedToOhterColumn) {
+    // update Query
+  }
 
   resetToDefault()
 }
@@ -50,15 +61,8 @@ const onMouseMove = (e: MouseEvent) => {
     return
   }
 
-  const floatingItem = document.querySelector('.float-item') as HTMLElement
+  setFloatingItem(e)
 
-  const { pageX, pageY } = e
-  floatingItem.style.left = pageX + position.x + 'px'
-  floatingItem.style.top = pageY + position.y + 'px'
-
-  //   document
-  //     .querySelectorAll('.item-wrapper.overlapped')
-  //     .forEach((item) => item.classList.remove('overlapped'))
   resetOverlapped()
   findOverlappedItem(e)
   findOverlappedColumn(e)
@@ -72,43 +76,37 @@ const onMouseMove = (e: MouseEvent) => {
   //   }
 }
 
-const resetOverlapped = () => {
-  if (overlappedElement.item) {
-    overlappedElement.item.classList.remove('overlapped')
-    overlappedElement.item = null
-  }
-  if (overlappedElement.column) {
-    overlappedElement.column.classList.remove('overlapped')
-    overlappedElement.column = null
-  }
-}
-
 const swapIfOverlappedWithItem = () => {
   if (!overlappedElement.item) {
-    return
+    return false
   }
-  const overlappedPostion = overlappedElement.item.getBoundingClientRect()
-  const targetPostion = target.getBoundingClientRect()
 
-  if (overlappedPostion.top < targetPostion.top) {
-    overlappedElement.item.insertAdjacentElement('beforebegin', target)
-  } else {
-    overlappedElement.item.insertAdjacentElement('afterend', target)
-  }
+  const parent = (overlappedElement.column ||
+    target.parentElement) as HTMLElement
+  parent.insertBefore(target, overlappedElement.item)
+  //   if (overlappedPostion.top < targetPostion.top) {
+  //     overlappedElement.item.insertAdjacentElement('beforebegin', target)
+  //   } else {
+  //     overlappedElement.item.insertAdjacentElement('afterend', target)
+  //   }
+
+  return true
 }
 
 const moveToOtherColumn = () => {
   if (overlappedElement.item || !overlappedElement.column) {
-    return
+    return false
   }
-
-  const container = document.querySelector('.column-items-container')
-  console.log(container)
-  container.appendChild(target)
+  console.log('added to column')
+  //   const container = document.querySelector('.column-items-container')
+  //   console.log(container)
+  overlappedElement.column.appendChild(target)
+  return true
 }
 
 const findOverlappedItem = (e: MouseEvent) => {
-  const elements = document.elementsFromPoint(e.pageX, e.pageY)
+  const { pageX, pageY } = e
+  const elements = document.elementsFromPoint(e.pageX, e.pageY) as HTMLElement[]
   const [overlappedItem, ..._] = elements.filter(
     (item) =>
       item !== cloned &&
@@ -127,19 +125,27 @@ const findOverlappedItem = (e: MouseEvent) => {
 const findOverlappedColumn = (e: MouseEvent) => {
   const elements = document.elementsFromPoint(e.pageX, e.pageY)
   const [overlappedColumn, ..._] = elements.filter(
-    (item) =>
-      item.classList.contains('column-container') &&
-      item.id !== target.parentElement.id
+    (item) => item.classList.contains('column-items-container') //&&item.id !== target.parentElement.id
   )
 
   overlappedElement.column = overlappedColumn
   if (!overlappedColumn) {
     return
   }
+  overlappedElement.column.classList.add('overlapped')
+}
 
-  console.log(`has overlapped column`)
-
-  //   overlapped.classList.add('overlapped')
+const resetOverlapped = () => {
+  if (overlappedElement.item) {
+    overlappedElement.item.style = null
+    overlappedElement.item.classList.remove('overlapped')
+    overlappedElement.item = null
+  }
+  if (overlappedElement.column) {
+    overlappedElement.column.style = null
+    overlappedElement.column.classList.remove('overlapped')
+    overlappedElement.column = null
+  }
 }
 
 const resetToDefault = () => {
@@ -153,10 +159,10 @@ const resetToDefault = () => {
 
   target = null
   cloned = null
-  originalLeft = null
-  originalTop = null
+  isOnTop = null
   resetOverlapped()
 }
+
 window.addEventListener('pointerdown', onMouseDown)
 window.addEventListener('pointerup', onMouseUp)
 window.addEventListener('pointermove', onMouseMove)
