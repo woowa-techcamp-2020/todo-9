ae9ef30974c1f7e0f1bf03da4e1bfd6f45aa36b6
naamoonoo <naamoonoo.y@gmail.com>
2020-07-23 10:33:59 +0900

    [#42] feat: api setup for item(CUD)

--- client/src/apis/item.ts
+++ client/src/apis/item.ts
@@ -1,7 +1,39 @@
 import { IUser } from './user'
 import { fetchWrapper } from '../utils/fetchWrapper'
-
+import { promiseHandler } from '../utils/promiseHandler'
 export interface IItem {
   id: number
   content: string
+}
+
+export const createItem = async (content: string): Promise<void> => {
+  const [_, error] = await promiseHandler(
+    fetchWrapper<IUser[], Partial<IItem>>('POST', `/item`, {
+      content,
+    })
+  )
+  if (error) {
+    console.error(error)
+  }
+}
+
+export const updateItem = async (item: IItem): Promise<void> => {
+  const [_, error] = await promiseHandler(
+    fetchWrapper<IUser[], Partial<IItem>>('PUT', `/item/${item.id}`, {
+      content: item.content,
+    })
+  )
+  if (error) {
+    console.error(error)
+  }
+}
+
+export const deleteItem = async (id: number): Promise<void> => {
+  const [_, error] = await promiseHandler(
+    fetchWrapper<IUser[], Partial<IItem>>('DELETE', `/item/${id}`)
+  )
+
+  if (error) {
+    console.error(error)
+  }
 }
