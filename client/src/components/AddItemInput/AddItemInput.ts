36b6463bbdfd199699517b84ad61713b4c0ffe5a
naamoonoo <naamoonoo.y@gmail.com>
2020-07-23 14:29:31 +0900

    [#42] feat: update props and state

--- client/src/components/AddItemInput/AddItemInput.ts
+++ client/src/components/AddItemInput/AddItemInput.ts
@@ -6,6 +6,8 @@ import { updateItem, createItem } from '../../apis/item'
 
 interface IProps {
   toggleAddItemInput: () => void
+  userId?: number
+  kanbanId?: number
   itemId?: number
   initialValue?: string
 }
@@ -23,27 +25,28 @@ class AddItemInput extends Component<IProps, IState> {
 
   async onSubmit(e: Event) {
     // 흐어어엉왜 안대지
-    const { itemId, initialValue } = this.props
+    const { itemId, kanbanId } = this.props
     const inputElement = this.element.querySelector(
       '.box-input'
     ) as HTMLTextAreaElement
-    const content = inputElement.textContent
-    if (itemId && initialValue) {
-      updateItem({ id: itemId, content })
+    const content = inputElement.value
+    if (itemId) {
+      await updateItem({ id: itemId, content })
     } else {
-      createItem(content)
+      await createItem({ kanbanId: kanbanId, content })
     }
+    await window.dispatchEvent(new Event('item_changed'))
   }
 
   render() {
     const inputBox = new BoxInput({
-      value: this.props.initialValue,
+      initialValue: this.props.initialValue || '',
       placeholder: 'Enter a note',
     })
 
     const addButton = new BoxButton({
       type: 'positive',
-      buttonText: this.props.initialValue ? 'Update' : 'Add',
+      buttonText: this.props.itemId ? 'Update' : 'Add',
       onClickHandler: (e: Event) => this.onSubmit(e),
       // disabed: this.getState('inputText').length === 0 || false,
     })
